26d32602f181198319518b1f65a9699753ff6537
r0meroh <romeroh862@gmail.com>
2023-01-07 22:57:44 -0600

    added queue, all queue funcs and print out func example

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"GOPATH/stack_n_queue"
-	
+	"fmt"
 )
 
 
 func main() {
-
+	fmt.Println("\n===================================================")
+	fmt.Println("Stack Example")
+	fmt.Println("===================================================\n")
 	stack_n_queue.PrintOutStackExample()
+	
+	fmt.Println("\n===================================================")
+	fmt.Println("Queue Example")
+	fmt.Println("===================================================\n")
 
+	stack_n_queue.ExampleQueue()
 }
---
+++ stack_n_queue/queue.go
@@ -0,0 +1,41 @@
+package stack_n_queue
+
+import "fmt"
+
+// queue
+type Queue struct {
+	elements []int
+}
+
+// enqueue
+func (myQueue *Queue) Enqueue(i int) {
+	myQueue.elements = append(myQueue.elements, i)
+}
+
+// dequeue
+func (myQueue *Queue) Dequeue() {
+	myQueue.elements = myQueue.elements[1:]
+}
+
+// return dequeue value
+func (myQueue *Queue) ReturnDequeue() int {
+	temp := myQueue.elements[0]
+	myQueue.Dequeue()
+	return temp
+}
+
+// print example usage
+func ExampleQueue() {
+	// stack_n_queue.PrintOutStackExample()
+	fmt.Println("Creating an empty Queue. Adding to it and removing values")
+	newQueue := Queue{}
+	fmt.Println(newQueue)
+	newQueue.Enqueue(1)
+	newQueue.Enqueue(2)
+	newQueue.Enqueue(3)
+	fmt.Printf("Added to empty queue. Current queue is: %v. Removing values\n", newQueue)
+	newQueue.Dequeue()
+	fmt.Println(newQueue)
+	fmt.Printf("Next value to be removed is: %v. \nFinal values in queue: ", newQueue.ReturnDequeue())
+	fmt.Println(newQueue)
+}
